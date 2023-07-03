543caaf29379179ad144066ba3307bd795168469
Aaftab <aaftabvhora62@gmail.com>
2023-07-03 17:49:32 -0400

    Organized the whole project and change the name of the package.

---
+++ app/src/main/java/com/example/rentalhub/Adapters/sellProductAdapter.java
@@ -0,0 +1,56 @@
+package com.example.rentalhub.Adapters;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.rentalhub.DataClass.sellProductModel;
+import com.example.rentalhub.R;
+import com.example.rentalhub.addProductActivity;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+public class sellProductAdapter extends FirebaseRecyclerAdapter<sellProductModel,sellProductAdapter.myviewHolder> {
+
+    public sellProductAdapter(@NonNull FirebaseRecyclerOptions<sellProductModel> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull sellProductAdapter.myviewHolder holder, int position, @NonNull sellProductModel model) {
+        holder.categoryName.setText(model.getCategory_name());
+        Glide.with(holder.circleImageView.getContext()).load(model.getCategory_Image_ID()).into(holder.circleImageView);
+        holder.circleImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.getContext().startActivity(new Intent(v.getContext(), addProductActivity.class));
+            }
+        });
+    }
+
+    @NonNull
+    @Override
+    public sellProductAdapter.myviewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_card, parent, false);
+        return new sellProductAdapter.myviewHolder(view);
+    }
+
+    class myviewHolder extends RecyclerView.ViewHolder {
+        ImageView circleImageView; //chnage
+        TextView categoryName;
+        public myviewHolder(@NonNull View itemView) {
+            super(itemView);
+            circleImageView = itemView.findViewById(R.id.category_image_ID);
+            categoryName = itemView.findViewById(R.id.category_name_ID);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/rentalhub/DataClass/Product_Model.java
@@ -0,0 +1,81 @@
+package com.example.rentalhub.DataClass;
+
+public class Product_Model {
+
+    private String Product_Name, Product_Rent, Product_City, P_image, Deposit, Product_Description, Fuel_Type, Sub_Category;
+
+    public Product_Model() {
+    }
+
+    public Product_Model(String product_Name, String product_Rent, String product_City, String p_image, String deposite) {
+        Product_Name = product_Name;
+        Product_Rent = product_Rent;
+        Product_City = product_City;
+        P_image = p_image;
+        Deposit = deposite;
+    }
+
+    public String getProduct_Name() {
+        return Product_Name;
+    }
+
+    public void setProduct_Name(String product_Name) {
+        Product_Name = product_Name;
+    }
+
+    public String getProduct_Rent() {
+        return Product_Rent;
+    }
+
+    public void setProduct_Rent(String product_Rent) {
+        Product_Rent = product_Rent;
+    }
+
+    public String getProduct_City() {
+        return Product_City;
+    }
+
+    public void setProduct_City(String product_City) {
+        Product_City = product_City;
+    }
+
+    public String getP_image() {
+        return P_image;
+    }
+
+    public void setP_image(String p_image) {
+        P_image = p_image;
+    }
+
+    public String getDeposit() {
+        return Deposit;
+    }
+
+    public void setDeposit(String deposit) {
+        Deposit = deposit;
+    }
+
+    public String getProduct_Description() {
+        return Product_Description;
+    }
+
+    public void setProduct_Description(String product_Description) {
+        Product_Description = product_Description;
+    }
+
+    public String getFuel_Type() {
+        return Fuel_Type;
+    }
+
+    public void setFuel_Type(String fuel_Type) {
+        Fuel_Type = fuel_Type;
+    }
+
+    public String getSub_Category() {
+        return Sub_Category;
+    }
+
+    public void setSub_Category(String sub_Category) {
+        Sub_Category = sub_Category;
+    }
+}
